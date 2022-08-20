1697c93051fe2b1772b2f25553e91fbd04485f5f
desai-dev <108363018+desai-dev@users.noreply.github.com>
2022-08-20 16:24:06 -0700

    Add files via upload

---
+++ public/home.js
@@ -0,0 +1,42 @@
+async function getAddress() {
+    let addressInput = document.getElementById("address-input").value;
+    data = {addressInput};
+
+    const options = {
+        method: 'POST',
+        headers: {
+           'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    const response = await fetch('/address', options);
+    const json = await response.json();
+    console.log(json.wattData);
+
+}
+
+// if ('geolocation' in navigator) {
+
+//     navigator.geolocation.getCurrentPosition(async position => {
+
+//         const api_url = 'http://api.positionstack.com/v1/forward?access_key=b613b9c534d825142cb3e1fa06088bcb&query=155 Paisley St, Guelph, ON&limit=1';
+//         const response = await fetch(api_url);
+//         const json = await response.json();
+
+        
+//         const data = {lon, lat};
+//         const options = {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type':'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         };
+
+
+//         console.log(json);
+//         console.log(data);
+//     });
+//   } else {
+//     console.log('not working');
+//   }
